24e23e5fb8cb48d293acb5d623f565ba35ef71a9
Rong "Mantle" Bao <webmaster@csmantle.top>
2026-01-01 22:10:42 +0800

    feat(backend): add cookies and conn pool
    Signed-off-by: Rong "Mantle" Bao <webmaster@csmantle.top>

--- backend/src/database/lib.rs
+++ backend/src/database/lib.rs
@@ -1,2 +1,17 @@
+#![deny(clippy::all)]
+
 pub mod model;
 pub mod schema;
+
+use diesel::prelude::*;
+use diesel::r2d2::{ConnectionManager, Pool};
+use dotenvy_macro::dotenv;
+
+pub fn get_conn_pool() -> Pool<ConnectionManager<PgConnection>> {
+    let manager = ConnectionManager::<PgConnection>::new(dotenv!("DATABASE_URL"));
+    Pool::builder()
+        .max_size(5)
+        .test_on_check_out(true)
+        .build(manager)
+        .expect("Could not build connection pool")
+}
--- backend/src/server/main.rs
+++ backend/src/server/main.rs
@@ -1,18 +1,45 @@
-use actix_web::middleware::Logger;
-use actix_web::{App, HttpServer};
+#![deny(clippy::all)]
+
+mod route;
+
+use actix_session::{SessionMiddleware, storage::CookieSessionStore};
+use actix_web::{App, HttpServer, cookie::Key, middleware::Logger, web};
+use argon2::Argon2;
 use dotenvy_macro::dotenv;
 use env_logger::Env;
 
-mod route;
+fn derive_cookie_key(passphrase: &str) -> Key {
+    let passphrase = passphrase.as_bytes();
+    let mut key_material = [0u8; 64];
+    Argon2::default()
+        .hash_password_into(
+            passphrase,
+            b"hdu2025_software_eng_thesis_mgmt/backend",
+            &mut key_material,
+        )
+        .expect("Failed to derive cookie key");
+    Key::from(&key_material)
+}
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     env_logger::init_from_env(Env::default().default_filter_or(dotenv!("RUST_LOG")));
 
-    HttpServer::new(|| {
+    let pool = backend_database::get_conn_pool();
+
+    HttpServer::new(move || {
         App::new()
             .wrap(Logger::default())
             .wrap(Logger::new("%a %{User-Agent}i"))
+            .wrap(
+                SessionMiddleware::builder(
+                    CookieSessionStore::default(),
+                    derive_cookie_key(dotenv!("COOKIE_PASSPHRASE")),
+                )
+                .cookie_secure(false)
+                .build(),
+            )
+            .app_data(web::Data::new(pool.clone()))
             .service(route::ping)
     })
     .bind((
